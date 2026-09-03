027ae0431a5fbf7e2bbc70d0c223d4de4728e58c
Zhuo Zhang <zhan3299@purdue.edu>
2026-09-03 10:51:47 -0400

    Add ProgramBench compile and test harness

---
+++ target/release/deps/shellharden-8040cd09c70389e9.d
@@ -0,0 +1,29 @@
+/Users/izhuer/Code/course-competition/shellharden/target/release/deps/shellharden-8040cd09c70389e9.d: src/main.rs src/machine.rs src/errfmt.rs src/filestream.rs src/situation.rs src/commonargcmd.rs src/commonstrcmd.rs src/microparsers.rs src/sitcase.rs src/sitcmd.rs src/sitcomment.rs src/sitextent.rs src/sitfor.rs src/sitmagic.rs src/sitrvalue.rs src/sitstrdq.rs src/sitstrphantom.rs src/sitstrsqesc.rs src/sittest.rs src/situntilbyte.rs src/sitvarbrace.rs src/sitvarident.rs src/sitvec.rs
+
+/Users/izhuer/Code/course-competition/shellharden/target/release/deps/shellharden-8040cd09c70389e9: src/main.rs src/machine.rs src/errfmt.rs src/filestream.rs src/situation.rs src/commonargcmd.rs src/commonstrcmd.rs src/microparsers.rs src/sitcase.rs src/sitcmd.rs src/sitcomment.rs src/sitextent.rs src/sitfor.rs src/sitmagic.rs src/sitrvalue.rs src/sitstrdq.rs src/sitstrphantom.rs src/sitstrsqesc.rs src/sittest.rs src/situntilbyte.rs src/sitvarbrace.rs src/sitvarident.rs src/sitvec.rs
+
+src/main.rs:
+src/machine.rs:
+src/errfmt.rs:
+src/filestream.rs:
+src/situation.rs:
+src/commonargcmd.rs:
+src/commonstrcmd.rs:
+src/microparsers.rs:
+src/sitcase.rs:
+src/sitcmd.rs:
+src/sitcomment.rs:
+src/sitextent.rs:
+src/sitfor.rs:
+src/sitmagic.rs:
+src/sitrvalue.rs:
+src/sitstrdq.rs:
+src/sitstrphantom.rs:
+src/sitstrsqesc.rs:
+src/sittest.rs:
+src/situntilbyte.rs:
+src/sitvarbrace.rs:
+src/sitvarident.rs:
+src/sitvec.rs:
+
+# env-dep:CARGO_PKG_VERSION=4.3.1
